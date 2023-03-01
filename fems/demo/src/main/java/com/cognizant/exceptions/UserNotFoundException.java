0d68c1e439059d031dc00925574c5c30a26317d6
luiscruz0555 <108365911+luiscruz0555@users.noreply.github.com>
2023-03-01 08:50:23 -0800

    Delete fems/demo directory

--- fems/demo/src/main/java/com/cognizant/exceptions/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.exceptions;
-
-public class UserNotFoundException extends Exception {
-
-    public UserNotFoundException (String err){
-        super(err);
-    }
-
-}
--- fems/demo/src/test/java/com/cognizant/integrationTests/FemsIntegrationTest.java
+++
@@ -1,4 +0,0 @@
-package com.cognizant.integrationTests;
-
-public class FemsIntegrationTest {
-}
